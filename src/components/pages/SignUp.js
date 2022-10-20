5a985faca87b8c4599b77c0cd3d1f3c61901b047
Zaregotto <Zaregotto307@gmail.com>
2022-10-20 13:42:09 +0300

    Registration

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Login} from "../login";
+import {Registration} from "../registration";
+
+import './pages.css'
 
 const SignUp = () => {
+    const [currentForm, setCurrentForm] = useState('login');
+
+    const toggleForm = (formName) => {
+        setCurrentForm(formName);
+    }
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="formLogin">
+                {
+                    currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Registration onFormSwitch={toggleForm} />
+                }
+            </div>
+        </>
     );
 };
 
